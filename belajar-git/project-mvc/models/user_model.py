63cedb887065f1de7d9981d61ddc19b1225dc80e
Delya Melviani <delyamelviani60@gmail.com>
2026-05-16 12:28:11 +0700

    [FEAT} Implementasi awal pola arsitektur MVC

---
+++ belajar-git/project-mvc/models/user_model.py
@@ -0,0 +1,5 @@
+# Mendefinisikan data user sederhana
+user_data = {
+    "name": "Delya Melviani",
+    "role": "Developer"
+}
---
+++ belajar-git/project-mvc/views/user_view.py
@@ -0,0 +1,4 @@
+def render_user(user):
+    print(f"=== Profil Pengguna ===")
+    print(f"Nama: {user['name']}")
+    print(f"Peran: {user['role']}")
