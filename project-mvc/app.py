29c341b75faf94f0143ae8f79b397c8cf6d29b0a
Delya Melviani <delyamelviani60@gmail.com>
2026-06-26 23:28:40 +0700

    [FEAT] Menambahkan komponen dashboard dan state management sederhana

---
+++ project-mvc/app.py
@@ -0,0 +1,29 @@
+from views.dashboard_component import render_dashboard
+
+# Simulasi State
+app_state = {
+    "items": [],
+    "is_loading": True
+}
+
+def update_state(new_data):
+    app_state["items"] = new_data
+    app_state["is_loading"] = False
+
+if __name__ == "__main__":
+    print("Loading data...")
+
+    # Tampilkan kondisi loading
+    render_dashboard(app_state["items"], app_state["is_loading"])
+
+    # Simulasi data masuk dari Backend
+    mock_data = [
+        {"id": 101, "name": "Produk A"},
+        {"id": 102, "name": "Produk B"}
+    ]
+
+    update_state(mock_data)
+
+    print("\nData berhasil dimuat:\n")
+
+    render_dashboard(app_state["items"], app_state["is_loading"])
---
+++ project-mvc/views/dashboard_component.py
@@ -0,0 +1,10 @@
+def render_dashboard(data_list, is_loading):
+    print("--- DASHBOARD APLIKASI ---")
+
+    if is_loading:
+        print("Mohon Tunggu...")
+    elif not data_list:
+        print("[!] Data Kosong. Silakan sinkronisasi dengan Backend.")
+    else:
+        for item in data_list:
+            print(f"- Item ID: {item['id']} | Nama: {item['name']}")
